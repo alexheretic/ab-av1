771e9827c42feed2bbf237544774a306f732bd1b
Alex Butler <alexheretic@gmail.com>
2023-03-16 09:45:02 +0100

    Support librav1e (#125)

--- src/command/args/encode.rs
+++ src/command/args/encode.rs
@@ -351,6 +351,8 @@ impl Encoder {
     pub fn default_max_crf(&self) -> f32 {
         match self.as_str() {
             "libx264" | "libx265" => 46.0,
+            // rav1e: use max -qp
+            "librav1e" => 255.0,
             // Works well for svt-av1
             _ => 55.0,
         }
