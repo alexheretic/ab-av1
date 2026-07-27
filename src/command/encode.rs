3fb9e4ea2a2d7a491942d14d690a5625de56a05c
Alex Butler <alexheretic@gmail.com>
2026-07-27 09:13:22 +0100

    Add tmp_output to temporary file tracking while encoding
    Remove probably-unreachable panic from tmp_output_name

--- src/command/encode.rs
+++ src/command/encode.rs
@@ -10,6 +10,7 @@ use crate::{
     process::FfmpegOut,
     temporary::{self, TempKind},
 };
+use anyhow::Context;
 use clap::Parser;
 use console::style;
 use indicatif::{HumanBytes, ProgressBar, ProgressStyle};
@@ -77,9 +78,6 @@ pub async fn run(
          Pass in `--overwrite-input` to allow this."
     );
 
-    // output is temporary until encoding has completed successfully
-    temporary::add(&output, TempKind::NotKeepable);
-
     if defaulting_output {
         let out = shell_escape::escape(output.display().to_string().into());
         bar.println(style!("Encoding {out}").dim().to_string());
@@ -105,7 +103,8 @@ pub async fn run(
         output.file_name().and_then(|n| n.to_str()).unwrap_or("")
     );
 
-    let tmp_output = tmp_output_name(&output);
+    let tmp_output = tmp_output_name(&output)?;
+    temporary::add(&tmp_output, TempKind::NotKeepable);
 
     let mut enc = ffmpeg::encode(
         enc_args,
@@ -138,10 +137,8 @@ pub async fn run(
     enc.wait().await?; // ensure process has exited
     bar.finish();
 
-    std::fs::rename(tmp_output, &output)?;
-
-    // successful encode, so don't delete it!
-    temporary::unadd(&output);
+    std::fs::rename(&tmp_output, &output)?;
+    temporary::unadd(&tmp_output);
 
     // print output info
     let output_size = fs::metadata(&output).await?.len();
@@ -193,15 +190,10 @@ pub fn default_output_name(input: &Path, encoder: &Encoder, is_image: bool) -> P
     input.with_extension(format!("{pre}.{ext}"))
 }
 
-pub fn tmp_output_name(output: &Path) -> PathBuf {
+pub fn tmp_output_name(output: &Path) -> anyhow::Result<PathBuf> {
     let mut tmp_prefix = OsString::from(".tmp.ab-av1-encoding.");
-    let file_name = output
-        .file_name()
-        .expect("We couldn't have gotten this far if there was no name to refer to it by");
-
-    tmp_prefix.extend([file_name]);
-
+    tmp_prefix.push(output.file_name().context("no output file name")?);
     let mut output = output.to_path_buf();
     output.set_file_name(tmp_prefix);
-    output
+    Ok(output)
 }
